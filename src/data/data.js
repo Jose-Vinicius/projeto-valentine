1d18502f437e2097e2c14883803f23e7703b2304
Jose vinicius francener sarmento <70924237+Jose-Vinicius@users.noreply.github.com>
2022-06-13 20:10:33 -0300

    :computer:  Update files

--- src/data/data.js
+++ src/data/data.js
@@ -56,9 +56,6 @@ export default [
         mensagem: "Por que me apaixonei tanto pelo teu sorriso?",
     },
     {
-        mensagem: "Por que me apaixonei tanto pelo teu sorriso?"
-    },
-    {
         mensagem: "Nos momentos tristes, meu amparo é seu abraço.",
     },
     {
@@ -68,7 +65,6 @@ export default [
         mensagem: "Encontrei em você a razão de viver."
     },
     {
-        
         mensagem: "Meu ponto fraco é você!"
     },
     {
@@ -81,14 +77,12 @@ export default [
         mensagem: "Se você quiser, o meu mundo é seu."
     },
     {
-        
         mensagem: "Se você quiser, o meu mundo é seu."
     },
     {
         mensagem: "Você trouxe mais sol para os meus dias."
     },
     {
-        
         mensagem: "Que nós, apesar de tudo, sejamos um só."
     },
     {
@@ -98,70 +92,36 @@ export default [
         mensagem: "Você me enganta todos os dias"
     },
     {
-        mensagem: "Adoro suas mamadas"
-    },
-    {
-        mensagem: "Adoro sua sentada"
-    },
-    {
-        mensagem: "Adoro suas quicada"
-    },
-    {
-        mensagem: "Da o cu para mim ?"
-    },
-    {
-        mensagem: "Adoro suas quicada"
-    },
-    {
         mensagem: "Amo quando me enche o saco"
-    },
-    {
-        mensagem: "Adoro suas quicada"
-    },
-    {
-        mensagem: "Adoro sua sentada"
     },
     {
         mensagem: "Prefiro um minuto ao seu lado do que uma vida inteira sem você."
     },
     {
-        
         mensagem: "Nossos dias possuem 24 horas. 16 penso em você e nas outras 8, sonho contigo."
     },
     {
-        
         mensagem: "Serei aquele que vai sussurrar o seu nome em seu ouvido, que vai te colocar na cama todas as noites, que vai te acordar todas as manhãs... Eu serei o seu anjo da guarda."
     },
     {
-        
         mensagem: "Cada vez que os nossos olhos se cruzam, posso tocar o céu."
     },
     {
-        
         mensagem: "Todo este tempo, você me ensinou que o meu sorriso depende do seu."
     },
     {
-
         mensagem: "Amar é compreender que somos dois indivíduos particulares que decidiram compartilhar uma vida singular."
     },
     {
-
         mensagem: "Desde que o cupido flechou o meu coração, não sigo um minuto sem perder a minha razão."
     },
     {
-
         mensagem: "Apenas diga que me ama e serei sua para toda a eternidade."
     },
     {
-
         mensagem: "Meu sangue, apaixonado por você, esquenta todo o meu corpo."
     },
     {
-
         mensagem: "Eu me apaixonei por você no momento em que olhei para os seus olhos. Hoje, esse amor se transformou em respeito, confiança e paz. Sou grato por ter você na minha vida, meu amor!"
     },
-    
-    
-    
-
 ]
